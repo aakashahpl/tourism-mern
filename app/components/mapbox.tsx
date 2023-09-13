f3b10a9a956d81eeb1cc8e34fa0572bb48fde185
Abhinav <abhinavprajapati2001@gmail.com>
2023-09-13 23:26:47 +0530

    Add mapbox

---
+++ app/components/mapbox.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React, { useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+
+// Replace with your Mapbox access token
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYWFrYXNocGF0ZWxhaHBsIiwiYSI6ImNsbWF2MnpkejBkeW8zcGpyNnZsZGs1ancifQ.1CZK6EwQfroKMlUqn1yobA";
+
+const MapboxMap = () => {
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map-container",
+      style: "mapbox://styles/aakashpatelahpl/clmddgl7701a201nz4qonbcta", // Replace with your desired map style
+      center: [77, 23], // Initial map center
+      zoom: 4, // Initial zoom level
+      projection: "globe",
+    });
+
+    return () => map.remove(); // Clean up when unmounting
+  }, []);
+
+  return (
+    <div>
+      <div className="flex">
+        <div className="border-2 border-red-600 w-1/2 h-screen"></div>
+        <div className="border-2 border-red-600 w-1/2 h-screen bg-gradient-to-r from-transparent to-black">
+          <div
+            id="map-container"
+            style={{ width: "100%", height: "100%" }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MapboxMap;
---
+++ app/components/navbar.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import "../globals.css";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="w-[90%] h-[5rem] border-2 border-red-900 flex justify-between ">
+        <div id="Logo" className="test-bdr py-2 flex px-2">
+          <Image
+            src={"/logo.png"}
+            alt="My Image"
+            width={60} // Set the width
+            height={60} // Set the height
+          />
+          <h1 className="text-white text-3xl px-2 py-2">India</h1>
+        </div>
+
+        <div>
+          <ul className=" flex justify-between gap-6 py-4 px-5 test-bdr text-white text-2xl">
+            <li className="">text</li>
+            <li className="">text</li>
+            <li className="">text</li>
+            <li className="">text</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
